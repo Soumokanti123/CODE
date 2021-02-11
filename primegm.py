f614085dfc92f892df8d7648e4275209f0d45d65
Soumokanti123 <76912681+Soumokanti123@users.noreply.github.com>
2021-02-11 21:53:01 +0530

    Add files via upload

---
+++ primegm.py
@@ -0,0 +1,25 @@
+def isp(n):
+    for i in range(2, n + 1):
+        if i>1:
+            for num in range(2,i):
+                if(i%num) == 0:
+                    return False
+    return True
+p =[i for i in range(1,1000001)]
+p[0] = 0
+p[1] = 0
+count = 0
+for i in range(2,1000001):
+    if isp(i):
+        count += 1
+    p.append(count)
+t=int(input())
+while t:
+    n,k=input().split()
+    n = int(n)
+    k = int(k)
+    if p[n] > k:
+        print("Divyam\n")
+    else:
+        print("Chef\n")
+    t -= 1
